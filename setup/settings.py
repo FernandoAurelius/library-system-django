6584a57db56a3c444eb280931ae753193be62148
Miguel Fernando Flores <miguelfernandoaurelius@gmail.com>
2024-11-18 11:36:40 -0300

    feat(requirements.txt): add psycopg2-binary for PostgreSQL database support
    feat(settings.py): configure PostgreSQL database settings using environment variables.  Switch from SQLite to PostgreSQL.

--- setup/settings.py
+++ setup/settings.py
@@ -51,20 +51,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "setup.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': str(os.getenv('DB_NAME')),
+        'USER': str(os.getenv('DB_USER')),
+        'PASSWORD': str(os.getenv('DB_PASSWORD')),
+        'HOST': str(os.getenv('DB_HOST')),
+        'PORT': str(os.getenv('DB_PORT')),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
